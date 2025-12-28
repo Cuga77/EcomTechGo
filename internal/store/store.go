cdd36d60d487523a3f615a5c92a7c2bcb7df91c0
Cuga77 <bogatov2021@mail.ru>
2025-12-29 01:32:16 +0300

    Добавил логировнние через slog, и возможность сохранять задачи в todos.json

--- internal/store/store.go
+++ internal/store/store.go
@@ -3,25 +3,71 @@ package store
 import (
 	"EcomTechGo/internal/models"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
 	"sync"
 )
 
 type Store struct {
-	Todos  map[int]models.Todo
-	mu     sync.Mutex
-	NextID int
+	Todos    map[int]models.Todo
+	mu       sync.Mutex
+	NextID   int
+	FilePath string
 }
 
 var ErrNotFound = errors.New("todo not found")
 
 func New() *Store {
-	storage := Store{
+	return &Store{
 		Todos: make(map[int]models.Todo),
 	}
+}
 
-	return &storage
+func (s *Store) save() error {
+	if s.FilePath == "" {
+		return nil
+	}
+	// Lock is already held by caller
+	data, err := json.MarshalIndent(s.Todos, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(s.FilePath, data, 0644)
+}
+
+func (s *Store) Save() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.save()
+}
+
+func (s *Store) Load(filename string) error {
+	s.FilePath = filename
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := json.Unmarshal(data, &s.Todos); err != nil {
+		return err
+	}
+	// Recalculate NextID
+	maxID := 0
+	for id := range s.Todos {
+		if id > maxID {
+			maxID = id
+		}
+	}
+	s.NextID = maxID
+	return nil
 }
 
 func (s *Store) Create(ctx context.Context, todo *models.Todo) error {
@@ -36,7 +82,7 @@ func (s *Store) Create(ctx context.Context, todo *models.Todo) error {
 	todo.ID = s.NextID
 	s.Todos[s.NextID] = *todo
 
-	return nil
+	return s.save()
 }
 
 func (s *Store) GetAll(ctx context.Context) ([]models.Todo, error) {
@@ -71,7 +117,7 @@ func (s *Store) Delete(ctx context.Context, ID int) error {
 	}
 
 	delete(s.Todos, ID)
-	return nil
+	return s.save()
 }
 
 func (s *Store) Update(ctx context.Context, ID int, todo *models.Todo) error {
@@ -88,5 +134,5 @@ func (s *Store) Update(ctx context.Context, ID int, todo *models.Todo) error {
 
 	todo.ID = ID
 	s.Todos[ID] = *todo
-	return nil
+	return s.save()
 }
